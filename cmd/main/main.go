6b2d2c6d74e5d9c7eaa7ff96203133343879ab5b
Julien BONACHERA <jbonachera@xebia.fr>
2017-05-01 19:17:53 +0200

    rename module name (typo) and use RF69

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/jbonachera/weathercontoller/config"
-	"github.com/jbonachera/weathercontoller/homie"
+	"github.com/jbonachera/weathercontroller/config"
+	"github.com/jbonachera/weathercontroller/homie"
+	"github.com/jbonachera/weathercontroller/radio"
 	"os"
 	"os/signal"
 )
@@ -14,12 +15,17 @@ func main() {
 
 	config.LoadDefaults()
 	homieClient := homie.NewClient("devices/", "172.20.0.100", 1883, false, false, "weatherStation")
+	radioClient := radio.NewClient(100, 1, func(metric radio.Metric) {
+		fmt.Println(metric)
+	})
 	homieClient.Start()
-	fmt.Println("connected to mqtt broker")
+	radioClient.Start("azertyuiopqsdfgh", "433")
+	fmt.Println("Subsystems booted")
 	select {
 	case <-sigc:
 		fmt.Println("received interrupt - aborting operations")
 		homieClient.Stop()
+		radioClient.Stop()
 		break
 	}
 	fmt.Println("main process finished")
